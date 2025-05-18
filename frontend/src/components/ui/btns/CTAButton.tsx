41c2008b35808e15c4b45ac6085802c14d695a9b
MaxCherya <maxchergik@gmail.com>
2025-05-18 17:47:33 +0300

    CTAButton - Added simple parametrs like: type, classname, label, onClick - Implemented simple designing for different types of button - Ready to use

--- frontend/src/components/ui/btns/CTAButton.tsx
+++ frontend/src/components/ui/btns/CTAButton.tsx
@@ -1,10 +1,27 @@
 import React from "react";
 
-const CTAButton: React.FC = () => {
+interface Props {
+    label?: string,
+    type: 'order' | 'learn' | 'contact',
+    className?: string,
+    onClick?: () => void;
+}
+
+const CTAButton: React.FC<Props> = ({ label = '', type, className = '', onClick }) => {
+
+    const styles = {
+        "order": "bg-blue-500 hover:bg-blue-600 text-white",
+        "learn": "bg-white hover:bg-gray-50 text-gray-800",
+        "contact": "bg-green-500 hover:bg-green-600 text-white"
+    }
+
     return (
-        <div>
-            CTAButton
-        </div>
+        <button onClick={onClick}
+            className={`${className} 
+                     rounded-sm px-2 py-1 cursor-pointer
+                     transition-all duration-150 ${styles[type]}`}>{
+                label}
+        </button>
     )
 }
 
