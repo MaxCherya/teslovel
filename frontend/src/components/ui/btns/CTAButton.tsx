0972e1af84a972eecc531d789c4b5071a67c1c2e
MaxCherya <maxchergik@gmail.com>
2025-05-18 18:01:35 +0300

    CTAButton patch - Added more eye-catchy design

--- frontend/src/components/ui/btns/CTAButton.tsx
+++ frontend/src/components/ui/btns/CTAButton.tsx
@@ -1,25 +1,24 @@
 import React from "react";
 
 interface Props {
-    label?: string,
-    type: 'order' | 'learn' | 'contact',
-    className?: string,
+    label?: string;
+    type: 'order' | 'learn';
+    className?: string;
     onClick?: () => void;
 }
 
 const CTAButton: React.FC<Props> = ({ label = '', type, className = '', onClick }) => {
 
     const styles = {
-        "order": "bg-blue-500 hover:bg-blue-600 text-white",
-        "learn": "bg-white hover:bg-gray-50 text-gray-800",
-        "contact": "bg-green-500 hover:bg-green-600 text-white"
+        "order": "bg-white text-black hover:bg-blue-500 hover:text-white",
+        "learn": "bg-gray-400 hover:bg-gray-700 text-gray-800 hover:text-white"
     }
 
     return (
         <button onClick={onClick}
             className={`${className} 
-                     rounded-sm px-2 py-1 cursor-pointer
-                     transition-all duration-150 ${styles[type]}`}>{
+                     rounded-full px-2 py-1 cursor-pointer
+                     transition-all duration-500 ${styles[type]}`}>{
                 label}
         </button>
     )
