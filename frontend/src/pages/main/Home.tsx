3af367e2ea427459b03584161991a8c056c82bec
MaxCherya <maxchergik@gmail.com>
2025-06-09 21:00:57 +0300

    ADDED CTA phone

--- frontend/src/pages/main/Home.tsx
+++ frontend/src/pages/main/Home.tsx
@@ -7,6 +7,8 @@ import CardsSwiper from "../../components/ui/swipers/CardsSwiper";
 import { useOutletContext } from 'react-router-dom'
 import FAQAccordion from "../../components/ui/accordions/FAQAccordion";
 import CTACallMe from "../../components/forPages/Home/CTACallMe";
+import { FiPhoneCall } from 'react-icons/fi';
+import { motion } from 'framer-motion';
 
 type ContextType = {
     showContacts?: boolean;
@@ -81,6 +83,32 @@ const Home: React.FC = () => {
             >
                 <FAQAccordion />
             </div>
+
+            <motion.div
+                className="fixed bottom-6 left-6 z-20"
+                animate={{ y: [0, -5, 0] }}
+                transition={{
+                    duration: 1.5,
+                    repeat: Infinity,
+                    ease: 'easeInOut',
+                }}
+            >
+                {/* Mobile (callable) */}
+                <a
+                    href="tel:+380630231663"
+                    className="lg:hidden flex items-center gap-2 p-3 rounded-full bg-black text-white shadow-lg"
+                >
+                    <FiPhoneCall className="w-6 h-6" />
+                </a>
+
+                {/* Desktop (show number) */}
+                <div className="hidden lg:flex items-center gap-2 p-3 bg-white border border-gray-300 rounded-full shadow-lg">
+                    <div className="p-2 bg-black text-white rounded-full">
+                        <FiPhoneCall className="w-5 h-5" />
+                    </div>
+                    <span className="text-black font-medium text-sm">+380 (63) 023 1663</span>
+                </div>
+            </motion.div>
 
             {/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d135292.82687010436!2d34.90397680762348!3d48.47980438746061!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa6897b741c5cb7a3%3A0xbf41530246ab3a17!2sTeslovel!5e1!3m2!1sen!2sua!4v1748258851933!5m2!1sen!2sua" className="w-screen h-[30rem]" style={{ border: 0 }} allowFullScreen loading="lazy"></iframe> */}
 
