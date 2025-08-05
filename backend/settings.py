4055b92a6603c10625d3e69257879d846f2426cf
Max <maxchergik@gmail.com>
2025-08-05 10:49:31 +0300

    Version 1.2.0 removed /assests/ from staticfiles dir

--- backend/settings.py
+++ backend/settings.py
@@ -175,7 +175,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'frontend' / 'dist' / 'assets',
+    BASE_DIR / 'frontend' / 'dist',
 ]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
