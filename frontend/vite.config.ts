ab34f5092c47696c544bbcaec55507be2b8c683c
Max <maxchergik@gmail.com>
2025-08-05 10:46:48 +0300

    Version 1.1.9 Again relative path

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,13 +3,13 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig(() => ({
-  base: '/static/assets/',
+  base: '/static/',
   plugins: [
     react(),
     tailwindcss(),
   ],
   build: {
-    outDir: 'dist/assets',
+    outDir: 'dist',
     emptyOutDir: true,
   },
 }))
